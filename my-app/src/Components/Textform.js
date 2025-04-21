dabbd9a5111515b27697ac54e60167c0fd386fc5
Rajat <rajatkumar17141@gmail.com>
2025-04-21 22:52:34 +0530

    Update Textform.js
    update code

--- my-app/src/Components/Textform.js
+++ my-app/src/Components/Textform.js
@@ -31,19 +31,19 @@ export default function Textform(props) {
     // setText("New Text") //Correct way to change state
   return (
     <>
-    <div className="container"  style={{color: props.mode==="dark"?"white":"black"}}>
+    <div className="container"  style={{color: props.textColor}}>
     <h1 >{props.heading}</h1>
   <div className="mb-3">
-    <textarea className="form-control" style={{backgroundColor: props.mode==="dark"?"grey":"white",color: props.mode==="dark"?"white":"black"}} value={text} onChange={handleonChange} id="Mybox" rows="8"></textarea>
+    <textarea className="form-control" style={{backgroundColor: props.textArea ,color: props.textColor}} value={text} onChange={handleonChange} id="Mybox" rows="8"></textarea>
   </div>
-  <button className="btn btn-primary mx-2" onClick={handleUPClick}>Convert to UpperCase</button>
-  <button className="btn btn-primary mx-1" onClick={handleloClick}>Convert to LowerCase</button>
+  <button className={`btn btn-${props.btnColor} mx-2`} onClick={handleUPClick}>Convert to UpperCase</button>
+  <button className={`btn btn-${props.btnColor} mx-1`} onClick={handleloClick}>Convert to LowerCase</button>
   <button className="btn btn-danger mx-2" onClick={handleClearClick}>Clear</button>
 
   </div>
-  <div className="container my-3"  style={{color: props.mode==="dark"?"white":"black"}}>
+  <div className="container my-3"  style={{color: props.textColor}}>
     <h2>Your Text Summary</h2>
-    <p>{text.split("  ").length} words and {text.length} characters</p>
+    <p>{text.split(" ").length-1} words and {text.length-0} characters</p>
     <p>{ 0.08*text.split(" ").length} Minutes read</p>
     
     <h2>Preview </h2>
